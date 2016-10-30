539ff00a355b82fd26d8d8613cce3d6b78fc4672
kolardia <kolardia@localhost.localdomain>
2016-10-30 19:28:33 +0100

    update a service-domain module

---
+++ service-domain/src/main/java/pl/service/science/publication/domain/Publication.java
@@ -0,0 +1,28 @@
+package pl.service.science.publication.domain;
+
+public class Publication {
+	
+	public int id;
+	public String title;
+	public String contents;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+}
