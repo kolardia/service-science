a85c698691b1af2ea59a3ac9e9756ec2d504fbe9
kolardia <kolardia@gmail.com>
2017-03-23 19:31:29 +0100

    documentation test

--- service-application/src/main/java/pl/service/science/translation/service/ServiceLanguage.java
+++ service-application/src/main/java/pl/service/science/translation/service/ServiceLanguage.java
@@ -7,9 +7,20 @@ import pl.service.science.translation.domain.Language;
 @Service
 public interface ServiceLanguage {
 
+	/**
+	 * @param id
+	 * @return
+	 */
 	public Language findById(Long id);
 
+	/**
+	 * @param code
+	 * @return
+	 */
 	public Language findByCode(String code);
 
+	/**
+	 * @param language
+	 */
 	public void save(Language language);
 }
